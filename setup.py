4dc4e253d38141bd7f02bd01a565759050fc1fc8
Lalit Patidar <42076213+LalitPatidar@users.noreply.github.com>
2019-12-11 15:29:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from Cython.Build import cythonize
 #import os
 #path_src = os.path.dirname(os.path.abspath(__file__)) + '/src'
 
+#os.system('export CFLAGS="-I /opt/aci/sw/python/3.6.3_anaconda-5.0.1/lib/python3.6/site-packages/numpy/core/include $CFLAGS"')
 extensions = [
     Extension("TGA",
                sources = ["TGA.pyx","Phase.cpp","Species.cpp", "Reactions.cpp"],
@@ -17,7 +18,8 @@ setup(
       name = "TGA_ext",
       ext_modules = cythonize(extensions,annotate=True),   
 )
-extensions = [
+
+extensions = [
     Extension("CRT",
                sources = ["CRT.pyx","Phase.cpp","Species.cpp", "Reactions.cpp"],
                language = "c++",
